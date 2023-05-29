ad2676cb915aac0cc38ae219118ffc44f65f12b1
adamritter <58403584+adamritter@users.noreply.github.com>
2023-05-29 20:32:49 +0200

    Improve formula

--- Formula/fastgron.rb
+++ Formula/fastgron.rb
@@ -1,21 +1,13 @@
-# Documentation: https://docs.brew.sh/Formula-Cookbook
-#                https://rubydoc.brew.sh/Formula
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class Fastgron < Formula
-  desc "fastgron is a high-performance JSON to GRON converter"
+  desc "High-performance JSON to GRON converter"
   homepage "https://github.com/adamritter/fastgron"
-  url "https://github.com/adamritter/fastgron/archive/refs/tags/v0.1.8.zip"
-  version "0.1.8"
+  url "https://github.com/adamritter/fastgron/archive/refs/tags/v0.1.8.tar.gz"
   sha256 "b281e85bcef395586086e5fbf508f4ac63781e7b57d196c1683da891b6c30a5d"
   license "MIT"
 
   depends_on "cmake" => :build
 
   def install
-    # ENV.deparallelize  # if your formula fails when building in parallel
-    # Remove unrecognized options if warned by configure
-    # https://rubydoc.brew.sh/Formula.html#std_configure_args-instance_method
-    # system "./configure", *std_configure_args, "--disable-silent-rules"
     system "cmake", "-S", ".", "-B", "build", *std_cmake_args
     system "cmake", "--build", "build"
     bin.install "build/fastgron"
