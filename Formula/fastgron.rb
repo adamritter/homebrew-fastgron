568103dbc793184afbe8298a309320f64e169e32
adamritter <58403584+adamritter@users.noreply.github.com>
2023-05-31 16:12:40 +0200

    Update fastgron.rb

--- Formula/fastgron.rb
+++ Formula/fastgron.rb
@@ -15,15 +15,7 @@ class Fastgron < Formula
   end
 
   test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! For Homebrew/homebrew-core
-    # this will need to be a test that verifies the functionality of the
-    # software. Run the test with `brew test fastgron`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system "#{bin}/program", "do", "something"`.
-    system "false"
+    assert_match "fastgron", shell_output("#{bin}/fastgron --version")
+    assert_match "json[1] = 4;", shell_output("echo '[3,4,5]' | #{bin}/fastgron")
   end
 end
